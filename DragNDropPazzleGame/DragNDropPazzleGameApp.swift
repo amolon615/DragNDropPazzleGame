3c1ac26ff454f508d1d7992a8c8037a86699920d
amolon615 <114776102+amolon615@users.noreply.github.com>
2023-02-22 22:49:24 +0100

    Initial Commit

---
+++ DragNDropPazzleGame/DragNDropPazzleGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragNDropPazzleGameApp.swift
+//  DragNDropPazzleGame
+//
+//  Created by Artem on 22/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct DragNDropPazzleGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
